e2bc1eee15493a95f46153b66540d04051ca8012
Michaela Markova <michaela.markov@seznam.cz>
2022-04-30 12:50:22 +0200

    added documentation comments

--- view.cpp
+++ view.cpp
@@ -1,5 +1,8 @@
 #include "view.hpp"
 
+/**
+ * @brief Prints help text to std::out
+ */
 void View::PrintHelp(){
     std::ifstream f("howToUse.txt");
 
@@ -7,6 +10,10 @@ void View::PrintHelp(){
         std::cout << f.rdbuf();
     }
 }
+/**
+ * @brief Prints to std:out, whether the operation was successful or not
+ * @param success true if operation was successful, false if not
+ */
 void View::PrintOperationResult( bool success){
     if(success){
         cout << "Operation Successful. " << endl;
@@ -15,11 +22,16 @@ void View::PrintOperationResult( bool success){
         cout << "Operation unsuccessful, please repeat the operation. " << endl;
     }
 }
+/**
+ * @brief Displays detailed information about budget and its categories
+ * @param budget Budget to display
+ */
 void View::PrintBudgetInfo(dataNS::Budget budget){
     double totalIncome = 0;
     double totalExpenditure = 0;
     double totalBudgeted = 0;
     cout << "Categories: " << endl;
+    //category handling
     for(const auto &myPair : budget.categories){
         totalBudgeted += myPair.second.budgeted;
         double expenseInCategory = 0;
@@ -27,23 +39,27 @@ void View::PrintBudgetInfo(dataNS::Budget budget){
             expenseInCategory += expense.amount;
         }
         totalExpenditure += expenseInCategory;
+        //printing categori information
         cout << "   " << myPair.first << endl;
         cout << "       Budgeted: " << myPair.second.budgeted << endl; 
         cout << "       Expense: " << expenseInCategory << endl;
         cout << "       Left to spend: " << myPair.second.budgeted - expenseInCategory << endl;
 
     }
-
     for(const auto &income : budget.incomes) // access by reference to avoid copying
     {
         totalIncome += income.amount;
     }
-
+    //printing budget information
     cout << "Total income: " << totalIncome << endl;
     cout << "Total expenditure: " << totalExpenditure << endl;
     cout << "Total budgeted: " << totalBudgeted << endl;
     cout << "Left to budget: " << totalIncome - totalBudgeted << endl;
 }
+/**
+ * @brief Prints detailed information about category end expenses within
+ * @param category category to display
+ */
 void View::PrintCategoryInfo(dataNS::Category category){
     double expenseInCategory = 0;
     cout << "Expenses: " << endl;
@@ -55,9 +71,13 @@ void View::PrintCategoryInfo(dataNS::Category category){
     cout << "Expense: " << expenseInCategory << endl;
     cout << "Left to spend: " << category.budgeted - expenseInCategory << endl;
 }
+/**
+ * @brief Prints list of all budgets with marker, which budget it the primary one
+ * @param budgets Budgets holder
+ */
 void View::PrintBudgetsList(dataNS::BudgetsHolder budgets){
     if(budgets.primaryBudgetName == "" && budgets.otherBudgets.size() == 0){
-        cout << "No budgets specified. " << endl;
+        cout << "No budgets were added yet. Use add_budget <budget_name>. " << endl;
     }
     else{
         cout << budgets.primaryBudgetName << " - primary budget" << endl;
