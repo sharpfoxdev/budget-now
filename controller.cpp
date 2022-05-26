8acf16dfdd07ed0d0508313d62035e3a4ec8a1be
Michaela Markova <michaela.markov@seznam.cz>
2022-05-27 00:03:20 +0200

    Updated handling of errors

--- controller.cpp
+++ controller.cpp
@@ -27,8 +27,12 @@ unique_ptr<IView> BudgetController::HandleRequest(string command, const vector<s
                 try{
                     budget = model.GetBudget(params);
                 }
+                catch (const std::exception &exc)
+                {
+                    return make_unique<MessageView>(exc.what());
+                }
                 catch(...){
-                    return;
+                    return make_unique<MessageView>("Unknown error. ");
                 }
                 return make_unique<BudgetInfoView>(budget);
                 break;
@@ -75,8 +79,12 @@ unique_ptr<IView> CategoryController::HandleRequest(string command, const vector
                 try{
                     category = model.GetCategory(params);
                 }
+                catch (const std::exception &exc)
+                {
+                    return make_unique<MessageView>(exc.what());
+                }
                 catch(...){
-                    return;
+                    return make_unique<MessageView>("Unknown error. ");
                 }
                 return make_unique<CategoryInfoView>(category);
                 break;
@@ -142,93 +150,93 @@ unique_ptr<IView> HelpController::HandleRequest(string command, const vector<str
     return make_unique<HelpView>(f);
 }
 
-/**
- * @brief Calls view to print help text to CLI
- */
-void Controller::PrintHelp(){
-    view.PrintHelp();
-}
+// /**
+//  * @brief Calls view to print help text to CLI
+//  */
+// void Controller::PrintHelp(){
+//     view.PrintHelp();
+// }
 
-/**
- * @brief Calls model to set primary budget and then calls view to display results. 
- * @param params primary budget parameters
- */
-void Controller::SetPrimaryBudget(const vector<string> & params){
-    bool success = model.SetPrimaryBudget(params);
-    view.PrintOperationResult(success);
-}
-/**
- * @brief Gets BudgetHolder from Model and then calls View to display it
- */
-void Controller::ListAllBudgets(){
-    dataNS::BudgetsHolder budgets = model.GetBudgetsHolder();
-    view.PrintBudgetsList(budgets);
-}
-/**
- * @brief Calls Model to copy old budget information into new one. 
- * @param params 
- */
-void Controller::CopyBudget(const vector<string> & params){
-    bool success = model.CopyBudget(params);
-    view.PrintOperationResult(success);
-}
-/**
- * @brief Adds budget into Model and displays result
- * @param params Budget parameters
- */
-void Controller::AddBudget(const vector<string> & params){
-    bool success = model.AddBudget(params);
-    view.PrintOperationResult(success);
-}
-/**
- * @brief Adds Expense into Model and displays result through View
- * @param params Expense parameters
- */
-void Controller::AddExpense(const vector<string> & params){
-    bool success = model.AddExpense(params);
-    view.PrintOperationResult(success);
-}
-/**
- * @brief Adds category into Model and displays result through View
- * @param params Category params
- */
-void Controller::AddCategory(const vector<string> & params){
-    bool success = model.AddCategory(params);
-    view.PrintOperationResult(success);
-}
-/**
- * @brief Adds income into Model and displays result through View
- * @param params Income params
- */
-void Controller::AddIncome(const vector<string> & params){
-    bool success = model.AddIncome(params);
-    view.PrintOperationResult(success);
-}
-/**
- * @brief Gets budget from Model and calls View to display it. 
- * @param params Budget params
- */
-void Controller::PrintBudgetInfo(const vector<string> & params){
-    dataNS::Budget budget;
-    try{
-        budget = model.GetBudget(params);
-    }
-    catch(...){
-        return;
-    }
-    view.PrintBudgetInfo(budget);
-}
-/**
- * @brief Gets Category from Model and calls View to display it. 
- * @param params Category params
- */
-void Controller::PrintCategoryInfo(const vector<string> & params){
-    dataNS::Category category;
-    try{
-        category = model.GetCategory(params);
-    }
-    catch(...){
-        return;
-    }
-    view.PrintCategoryInfo(category);
-}
+// /**
+//  * @brief Calls model to set primary budget and then calls view to display results. 
+//  * @param params primary budget parameters
+//  */
+// void Controller::SetPrimaryBudget(const vector<string> & params){
+//     bool success = model.SetPrimaryBudget(params);
+//     view.PrintOperationResult(success);
+// }
+// /**
+//  * @brief Gets BudgetHolder from Model and then calls View to display it
+//  */
+// void Controller::ListAllBudgets(){
+//     dataNS::BudgetsHolder budgets = model.GetBudgetsHolder();
+//     view.PrintBudgetsList(budgets);
+// }
+// /**
+//  * @brief Calls Model to copy old budget information into new one. 
+//  * @param params 
+//  */
+// void Controller::CopyBudget(const vector<string> & params){
+//     bool success = model.CopyBudget(params);
+//     view.PrintOperationResult(success);
+// }
+// /**
+//  * @brief Adds budget into Model and displays result
+//  * @param params Budget parameters
+//  */
+// void Controller::AddBudget(const vector<string> & params){
+//     bool success = model.AddBudget(params);
+//     view.PrintOperationResult(success);
+// }
+// /**
+//  * @brief Adds Expense into Model and displays result through View
+//  * @param params Expense parameters
+//  */
+// void Controller::AddExpense(const vector<string> & params){
+//     bool success = model.AddExpense(params);
+//     view.PrintOperationResult(success);
+// }
+// /**
+//  * @brief Adds category into Model and displays result through View
+//  * @param params Category params
+//  */
+// void Controller::AddCategory(const vector<string> & params){
+//     bool success = model.AddCategory(params);
+//     view.PrintOperationResult(success);
+// }
+// /**
+//  * @brief Adds income into Model and displays result through View
+//  * @param params Income params
+//  */
+// void Controller::AddIncome(const vector<string> & params){
+//     bool success = model.AddIncome(params);
+//     view.PrintOperationResult(success);
+// }
+// /**
+//  * @brief Gets budget from Model and calls View to display it. 
+//  * @param params Budget params
+//  */
+// void Controller::PrintBudgetInfo(const vector<string> & params){
+//     dataNS::Budget budget;
+//     try{
+//         budget = model.GetBudget(params);
+//     }
+//     catch(...){
+//         return;
+//     }
+//     view.PrintBudgetInfo(budget);
+// }
+// /**
+//  * @brief Gets Category from Model and calls View to display it. 
+//  * @param params Category params
+//  */
+// void Controller::PrintCategoryInfo(const vector<string> & params){
+//     dataNS::Category category;
+//     try{
+//         category = model.GetCategory(params);
+//     }
+//     catch(...){
+//         return;
+//     }
+//     view.PrintCategoryInfo(category);
+// }
--- controller.hpp
+++ controller.hpp
@@ -100,21 +100,21 @@ public:
 /**
  * @brief Calls Model to get/set data and then calls View to show them to the user on CLI
  */
-class Controller{
-public: 
-    void ListAllBudgets();
-    void CopyBudget(const vector<string> & params);
-    void AddBudget(const vector<string> & params);
-    void AddExpense(const vector<string> & params);
-    void AddCategory(const vector<string> & params);
-    void AddIncome(const vector<string> & params);
-    void SetPrimaryBudget(const vector<string> & params);
-    void PrintBudgetInfo(const vector<string> & params);
-    void PrintCategoryInfo(const vector<string> & params);
-    void PrintHelp();
-private:
-    View view;
-    Model model;
-};
+// class Controller{
+// public: 
+//     void ListAllBudgets();
+//     void CopyBudget(const vector<string> & params);
+//     void AddBudget(const vector<string> & params);
+//     void AddExpense(const vector<string> & params);
+//     void AddCategory(const vector<string> & params);
+//     void AddIncome(const vector<string> & params);
+//     void SetPrimaryBudget(const vector<string> & params);
+//     void PrintBudgetInfo(const vector<string> & params);
+//     void PrintCategoryInfo(const vector<string> & params);
+//     void PrintHelp();
+// private:
+//     View view;
+//     Model model;
+// };
 
 #endif
